f00e47296b19bb570c7e128495fe68c24d2405fa
rabadiyajeet1119-art <rabadiyajeet1119@gmail.com>
2026-06-08 11:45:19 +0530

    Update app.py

--- xeno_wb - Copy/app.py
+++ xeno_wb - Copy/app.py
@@ -174,4 +174,6 @@ def ask():
         return jsonify({"reply": "Connection Error."})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render ke liye dynamic port aur host 0.0.0.0 zaroori hai
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
